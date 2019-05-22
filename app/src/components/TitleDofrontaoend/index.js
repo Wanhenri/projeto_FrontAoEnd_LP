7ec12626271b5bdaefcaf0748db04b7749bc87e6
wanhenri <slipkorn.jk@gmail.com>
2019-05-22 12:58:17 -0300

    atualizado o estilo de color e font-size no TitleDofrontaoend

--- app/src/components/TitleDofrontaoend/index.js
+++ app/src/components/TitleDofrontaoend/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {Title, Strong} from "./styles";
 
-const TitleDofrontaoend = () => {
+const TitleDofrontaoend = () => (
     <Title>do <Strong> Front </Strong> ao <Strong> End </Strong></Title>
-};
+);
 
 export default TitleDofrontaoend;
--- app/src/components/TitleDofrontaoend/styles.js
+++ app/src/components/TitleDofrontaoend/styles.js
@@ -1,4 +1,10 @@
 import styled from "styled-components";
 
-export const Title = styled.h1``;
-export const Strong = styled.strong``;
+export const Title = styled.h1`
+    color: var(--color-second);
+    font-size: 50px;
+`;
+export const Strong = styled.strong`
+    color: var(--color-zero);
+    font-size: 75px;
+`;
