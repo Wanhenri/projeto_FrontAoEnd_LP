749592d27c6c965d9955e4457a99f8deba1c6884
wanhenri <slipkorn.jk@gmail.com>
2019-05-22 15:55:41 -0300

    acrescimo do TextCollab, Wrapper, VideoDofrontaoend e do SubtitleCollab; e suas import

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
+import { Wrapper } from './styles';
+
+import SubtitleCollab from '../../objects/SubtitleCollab';
 import TitleDofrontaoend from "../../components/TitleDofrontaoend";
+import VideoDofrontaoend from '../../components/VideoDofrontaoend';
+import TextCollab from '../../objects/TextCollab';
 
-const Home =() => <TitleDofrontaoend>do Front ao End</TitleDofrontaoend>;
+ const Home = () => (
+    <Wrapper>
+        <TitleDofrontaoend>do Front ao End</TitleDofrontaoend>
+        <SubtitleCollab>
+             Um guia completo para quem quer se tornar um Desenvolvedor Front End
+        </SubtitleCollab>
 
-export default Home;
+        <VideoDofrontaoend />
+
+        <TextCollab>
+            Quer receber novidades da próxima turma, só colocar seu email abaixo.
+        </TextCollab>
+    </Wrapper>
+ );
+
+ export default Home;
+
+// class Home extends Component {
+//     state = {  }
+//     render() { 
+//         return ( 
+//             <div>
+//                 <TitleDofrontaoend>do Front ao End</TitleDofrontaoend>
+//                 <SubtitleCollab>Um guia completo para quem quer se tornar um Desenvolvedor Front End</SubtitleCollab>
+//             </div>
+//          );
+//     }
+// }
+//  
+// export default Home;
