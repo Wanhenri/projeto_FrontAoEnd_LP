988e3cd35f7b0b45a5ca707624aaa9b498c71b59
wanhenri <slipkorn.jk@gmail.com>
2019-05-22 13:02:40 -0300

    modificado defindo ao um erro encontrado na utilização do <></>. Foi também acrescentado os import necessários

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+
 
 import Colors from "./styles/settings/colors";
+import Spacing from "./styles/settings/spacing";
+import Reset from "./styles/generic/reset"
+import Base from "./styles/base";
+
 import Home from "./pages/Home";
 
-const App = () => (
-    <>
+class App extends Component{
+        render() {
+                return(
+    <div>
         <Colors />
+        <Spacing />
+        <Reset />
+        <Base />
         <Home />
-    </>
-);
+    </div>
+        )};
+}
 
 export default App;
+
+
