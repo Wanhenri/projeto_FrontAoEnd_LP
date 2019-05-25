d7d97bfdecd9934147d600f86dcc182e4c1520e9
wanhenri <slipkorn.jk@gmail.com>
2019-05-25 02:32:20 -0300

    acrescentado o padding, color e placeholder

--- app/src/objects/InputCollab/index.js
+++ app/src/objects/InputCollab/index.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
 const InputCollab = styled.input`
-background-color: var(--color-negative-light);
+    background-color: var(--color-negative-light);
     width:190px;
     height:38px;
     border-radius: var(--radius-small);
+    padding-left: var(--spacing-small);
+    color:var(--color-zero);
+
+    &::placeholder {
+        color: var(--color-zero);
+        font-weight:bold;
+    }
 `;
 
 export default InputCollab;
