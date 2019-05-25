c5bfe283f37fe658ad2bd96452c8a8aa933498a2
wanhenri <slipkorn.jk@gmail.com>
2019-05-25 02:33:45 -0300

    acrescentado o Btn e os placeholder dos InputCollab

--- app/src/components/FormLead/index.js
+++ app/src/components/FormLead/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { Form }from "./styles";
+import { Form } from "./styles";
 import InputCollab from "../../objects/InputCollab";
+import BtnCollab from '../BtnCollab';
 
 const FormLead = () => (
     <Form>
-        <InputCollab />
-        <InputCollab />
+        <InputCollab placeholder="Nome"/>
+        <InputCollab placeholder="Email"/>
+
+        <BtnCollab content="Enviar"/>
     </Form>
 );
 
