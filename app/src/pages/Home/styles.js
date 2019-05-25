948eaf6599640304f608382899237fa518a738d2
wanhenri <slipkorn.jk@gmail.com>
2019-05-25 02:37:13 -0300

    Foi adicionado a Section, juntamente com a sua configuração

--- app/src/pages/Home/styles.js
+++ app/src/pages/Home/styles.js
@@ -2,7 +2,14 @@ import styled from "styled-components";
 
 export const Wrapper = styled.main `
     background-color: var(--color-first);
-    height: 100vh;
+    height:100vh;
 `;
 
-export default Wrapper;
+export const Section = styled.section`
+    width:550px;    
+    margin-left:auto;
+    margin-right:auto;
+    text-align:center;
+`;
+
+
