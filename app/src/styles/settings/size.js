925f75d2b214b75ce689c032f0efa168b74ea468
wanhenri <slipkorn.jk@gmail.com>
2019-05-27 23:29:51 -0300

    add size-medium, size-very-big, e a porcentagem

--- app/src/styles/settings/size.js
+++ app/src/styles/settings/size.js
@@ -3,7 +3,10 @@ import { createGlobalStyle} from "styled-components";
 const Size = createGlobalStyle `
     :root {
         --radius-small: 3px;
-        --size-small:16px;
+        --size-base:10px;
+        --size-small:14px;
+        --size-medium:18px;
+        --size-very-big:5em;
     }
 
 `
