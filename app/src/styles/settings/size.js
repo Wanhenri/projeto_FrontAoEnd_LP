7e0e61f6e90eb7a7d67fcc5cd175bf72d0efe3d5
wanhenri <slipkorn.jk@gmail.com>
2019-05-28 01:37:36 -0300

    modificação no size da font

--- app/src/styles/settings/size.js
+++ app/src/styles/settings/size.js
@@ -1,15 +1,17 @@
 import { createGlobalStyle} from "styled-components";
 
+const heightWindow = window.innerHeight;
+
+
 const Size = createGlobalStyle `
     :root {
-        --radius-small: 3px;
-        --size-base:10px;
-        --size-small:14px;
-        --size-medium:18px;
-        --size-very-big:5em;
+        --radius-small: 4px;
+        --size-base:calc(10vh * 100 / ${heightWindow});
+        --size-small:calc(14vh * 100 / ${heightWindow});
+        --size-medium:calc(18vh * 100 / ${heightWindow});
+        --size-very-big:calc(50vh * 100 / ${heightWindow});
     }
-
-`
+`;
 
 export default Size;
 
