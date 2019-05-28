080789d588b9b394b8a814f89448f145165dbbf8
wanhenri <slipkorn.jk@gmail.com>
2019-05-27 23:19:48 -0300

    acrescentando e modificando a font-size

--- app/src/components/TitleDofrontaoend/styles.js
+++ app/src/components/TitleDofrontaoend/styles.js
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 export const Title = styled.h1`
     color: var(--color-second);
-    font-size: 50px;
+    font-size: var(--size-very-big);
 `;
 export const Strong = styled.strong`
     color: var(--color-zero);
-    font-size: 75px;
+    font-size: 1.5em;
 `;
