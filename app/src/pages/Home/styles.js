6b66ff3873fc42d6509afa6244ca72abffd37167
wanhenri <slipkorn.jk@gmail.com>
2019-05-27 23:25:24 -0300

    acrescimo das margin do subtitlecollab e textcollab

--- app/src/pages/Home/styles.js
+++ app/src/pages/Home/styles.js
@@ -1,15 +1,40 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.main `
-    background-color: var(--color-first);
-    height:100vh;
-`;
+import SubtitleCollab from "../../objects/SubtitleCollab";
+
+import TextCollab from "../../objects/TextCollab";
+
+import VideoDofrontaoend from "../../components/VideoDofrontaoend"
 
 export const Section = styled.section`
     width:550px;    
     margin-left:auto;
     margin-right:auto;
     text-align:center;
+
+    &> ${SubtitleCollab},
+    &> ${VideoDofrontaoend} {
+        margin-bottom: var(--spacing-big);
+    }  
+
+    &> ${SubtitleCollab} {
+        width:90%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    &> ${TextCollab}{
+        text-align:left;
+        margin-bottom: var(--spacing-small);
+    }
 `;
 
+export const Wrapper = styled.main `
+    background-color: var(--color-first);
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+`;
 
