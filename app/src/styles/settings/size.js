43c9d9e0209ad1d949e4f7a86c2d7421b190866f
wanhenri <slipkorn.jk@gmail.com>
2019-05-29 01:25:34 -0300

    atualizado

--- app/src/styles/settings/size.js
+++ app/src/styles/settings/size.js
@@ -7,7 +7,7 @@ const Size = createGlobalStyle `
     :root {
         --radius-small: 4px;
         --size-base:calc(10vh * 100 / ${heightWindow});
-        --size-small:calc(14vh * 100 / ${heightWindow});
+        --size-small:calc(16vh * 100 / ${heightWindow});
         --size-medium:calc(18vh * 100 / ${heightWindow});
         --size-very-big:calc(50vh * 100 / ${heightWindow});
     }
