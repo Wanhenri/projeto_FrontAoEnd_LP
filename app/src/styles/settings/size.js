790cc196d2d973521c389b35f85d27dfdae9b597
wanhenri <slipkorn.jk@gmail.com>
2019-05-29 13:03:35 -0300

    colocado o em nas size e também o @media para os break points

--- app/src/styles/settings/size.js
+++ app/src/styles/settings/size.js
@@ -1,15 +1,20 @@
 import { createGlobalStyle} from "styled-components";
 
-const heightWindow = window.innerHeight;
-
 
 const Size = createGlobalStyle `
     :root {
         --radius-small: 4px;
-        --size-base:calc(10vh * 100 / ${heightWindow});
-        --size-small:calc(16vh * 100 / ${heightWindow});
-        --size-medium:calc(18vh * 100 / ${heightWindow});
-        --size-very-big:calc(50vh * 100 / ${heightWindow});
+
+        --size-base:10px;
+        --size-small:1.5em;
+        --size-medium:1.8em;
+        --size-very-big:5em;
+    }
+
+    @media (max-height:700px) {
+        :root{
+            --size-base:9.8px;
+        }
     }
 `;
 
