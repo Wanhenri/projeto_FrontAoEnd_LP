79ef8b1e7c3ca4fe42577e30ffa24b8073aeaf26
wanhenri <slipkorn.jk@gmail.com>
2019-05-29 13:05:23 -0300

    acrescimo de break point

--- app/src/styles/settings/spacing.js
+++ app/src/styles/settings/spacing.js
@@ -2,9 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 const Spacing = createGlobalStyle`
     :root {
-        --spacing-small:10px;
-        --spacing-big:40px;
+        --spacing-base:10px;
+        --spacing-small:calc(1 *--spacing-base);
+        --spacing-big: calc(4 * --spacing-base);
     }    
+
+    @media (max-height:700px){
+        :root{
+        --spacing-base:8px;
+
+        }
+    }
 `;
 
 export default Spacing;
