b768d099d95993ac81a5afde71f65c37170293e3
wanhenri <slipkorn.jk@gmail.com>
2019-05-29 16:36:46 -0300

    atualizado

--- app/src/styles/settings/colors.js
+++ app/src/styles/settings/colors.js
@@ -3,7 +3,7 @@ import { createGlobalStyle} from "styled-components";
 const Colors = createGlobalStyle`
     :root {
         --color-negative-light: rgba(0, 0, 0, 0.15);
-        --color-base: #3A4042;
+        --color-base: #3A4042;        
         --color-zero: #FFFF;
         --color-first:#F25A70;
         --color-second: #191637;
