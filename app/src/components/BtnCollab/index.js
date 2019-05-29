d915bd07e1f221a2e976232b273fdec52c8188dd
wanhenri <slipkorn.jk@gmail.com>
2019-05-29 15:33:09 -0300

    acrescentando algumas configurações do botão

--- app/src/components/BtnCollab/index.js
+++ app/src/components/BtnCollab/index.js
@@ -9,6 +9,15 @@ const BtnCollab = styled.input.attrs({
     border-radius: var(--radius-small);
     color:var(--color-first);
     font-weight:bold;
+    font-size:var(--size-small);
+    transition:background-color 200ms linear;
+    cursor: pointer;
+
+    &:hover {
+        background-color:var(--color-first);
+        color:var(--color-zero);
+        border:1px solid var(--color-zero);
+    }
 `;
 
 export default BtnCollab;
