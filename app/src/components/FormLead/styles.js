f111f5cdf3060286433bf531f1c826d53f935cbf
wanhenri <slipkorn.jk@gmail.com>
2019-06-01 15:44:35 -0300

    acréscimo da configuração do TextCollab

--- app/src/components/FormLead/styles.js
+++ app/src/components/FormLead/styles.js
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
-export const Form = styled.form`
-    display:flex;
-    justify-content: space-between;
+import InputCollab from "../../objects/InputCollab";
 
+import TextCollab from '../../objects/TextCollab';
+
+export const Form = styled.form`
+    &> ${TextCollab}{
+        margin-bottom: var(--spacing-small);
+    }
+
+    &> ${InputCollab}{
+        margin-right: var(--spacing-small);
+    }
 `;
