013325a7e64c5918cfe3d363fc50595a238674e7
wanhenri <slipkorn.jk@gmail.com>
2019-06-01 15:45:23 -0300

    retirada do TextCollab

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -3,9 +3,8 @@ import React from 'react';
 import { Wrapper, Section } from './styles';
 
 import SubtitleCollab from '../../objects/SubtitleCollab';
-import TextCollab from '../../objects/TextCollab';
 
-import HeaderCollab from "../../components/HeaderCollab"
+
 import TitleDofrontaoend from "../../components/TitleDofrontaoend";
 import VideoDofrontaoend from '../../components/VideoDofrontaoend';
 import FormLead from '../../components/FormLead';
@@ -14,8 +13,6 @@ import FormLead from '../../components/FormLead';
 
  const Home = () => (
     <Wrapper>
-        <HeaderCollab />
-
         <Section>
             <TitleDofrontaoend>do Front ao End</TitleDofrontaoend>
             <SubtitleCollab>
@@ -24,9 +21,6 @@ import FormLead from '../../components/FormLead';
 
             <VideoDofrontaoend />
 
-            <TextCollab>
-                Quer receber novidades da próxima turma, só colocar seu email abaixo.
-            </TextCollab>
             <FormLead />
         </Section>
     </Wrapper>
