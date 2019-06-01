fde1fe787c53b482d77d4b2bd5c14b35926b41f8
wanhenri <slipkorn.jk@gmail.com>
2019-06-01 15:43:08 -0300

    acrescimo do TextCollab

--- app/src/components/FormLead/index.js
+++ app/src/components/FormLead/index.js
@@ -4,8 +4,14 @@ import { Form } from "./styles";
 import InputCollab from "../../objects/InputCollab";
 import BtnCollab from '../BtnCollab';
 
+import TextCollab from '../../objects/TextCollab';
+
 const FormLead = () => (
     <Form>
+        <TextCollab>
+            Quer receber novidades da próxima turma, só colocar seu email abaixo.
+        </TextCollab>
+        
         <InputCollab placeholder="Nome"/>
         <InputCollab placeholder="Email"/>
 
