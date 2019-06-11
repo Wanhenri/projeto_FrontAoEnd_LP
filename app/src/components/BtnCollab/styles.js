7352b737a48cc30af91b36469885572dfb9d4b76
wanhenri <slipkorn.jk@gmail.com>
2019-06-10 21:53:27 -0300

    retirado a config do botão e deixado apenas a const Wrapper

--- app/src/components/BtnCollab/styles.js
+++ app/src/components/BtnCollab/styles.js
@@ -1,37 +1,6 @@
 import styled, {css} from "styled-components";
 
-export const Button = styled.button.attrs({
-    type: "submit"
+export const Wrapper = styled.div`
+    transform: scale(1);
+`; 
 
-})`
-    position: absolute;
-    right: 0;
-    bottom:0;
-    box-sizing:border-box;
-    background-color:var(--color-zero);
-    width:100%;
-    height:40px;    
-    border-radius: var(--radius-small);
-    color:var(--color-zero);
-    font-weight:bold;
-    font-size:var(--size-small);
-    cursor: pointer;
-    will-change:transform;
-    transform-origin:right;
-    transform: ${({loading}) => loading ? "scaleX(1)" : "scaleX(0.27)"};
-    transition: transform ${({loading}) => loading ? "400ms" : "100ms"} linear, box-shadow 100ms linear, width 300ms linear;
-    box-shadow: 0px 0px 0px var(--color-base);
-
-    ${({loading}) => !loading && css ` 
-            &focus,
-            &:hover {
-                transform:translateY(-3px) scaleX(0.27);
-                box-shadow: 0px 2px 4px var(--color-base);
-            }
-
-            &:active {
-                transform: translateY(-1px) scaleX(0.27);
-            }
-        `
-    }
-`;
