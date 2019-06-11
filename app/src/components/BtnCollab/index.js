150f3773ec3582bf74792bfbae6ef91622e6648d
wanhenri <slipkorn.jk@gmail.com>
2019-06-10 21:51:52 -0300

    configurado a parte de loading do botao

--- app/src/components/BtnCollab/index.js
+++ app/src/components/BtnCollab/index.js
@@ -1,9 +1,39 @@
-import React from "react";
+import styled, { css } from "styled-components";
 
-import {Button} from "./styles";
+const BtnCollab = styled.button.attrs({
+    type: "submit"
 
-const BtnCollab = props => (
-    <Button {...props} />
-);
+})`
+    position: absolute;
+    right: 0;
+    bottom:0;
+    box-sizing:border-box;
+    background-color:var(--color-zero);
+    width:${({loading}) => loading ? "100%" : "140px"};
+    height:40px;    
+    border-radius: var(--radius-small);
+    color:var(--color-first);
+    font-weight:bold;
+    font-size:var(--size-small);
+    cursor: pointer;
+    will-change:transform, width;
+    transition: transform 100ms linear,
+                box-shadow 100ms linear, 
+                width 600ms cubic-bezier(.94, -0.64,0,1);
+    box-shadow: 0px 0px 0px var(--color-base);
+
+    ${({loading}) => !loading && css ` 
+            &focus,
+            &:hover {
+                transform:translateY(-3px);
+                box-shadow: 0px 2px 4px var(--color-base);
+            }
+
+            &:active {
+                transform: translateY(-1px);
+            }
+        `
+    }
+`;
 
 export default BtnCollab;
