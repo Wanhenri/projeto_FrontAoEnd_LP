d33f9e24fc8699b3a59a525730529a716b3e26e6
wanhenri <slipkorn.jk@gmail.com>
2019-06-12 13:01:13 -0300

    chamando a figura com import

---
+++ app/src/objects/LogoAvatar/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import avatar from "../../img/logo-avatar.svg";
+
+const LogoAvatar = styled.img.attrs({
+    src: avatar
+})`
+
+`;
+
+export default LogoAvatar;
