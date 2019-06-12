18433875ac73c0f22065066d0b31ee7225f5eeef
wanhenri <slipkorn.jk@gmail.com>
2019-06-12 12:08:48 -0300

    criado o styles do HeaderCollab

---
+++ app/src/components/HeaderCollab/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    background-color: var(--color-zero);
+    width: 80px;    
+    height: 100vh;
+    position: absolute;
+    z-index:1;
+    top:0;
+    left:0;
+`;
+
