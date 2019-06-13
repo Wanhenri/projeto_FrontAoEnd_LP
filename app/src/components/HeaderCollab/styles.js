ca194375c7d8cd571ae0617be78015c51722291a
wanhenri <slipkorn.jk@gmail.com>
2019-06-12 21:23:34 -0300

    atualizado

--- app/src/components/HeaderCollab/styles.js
+++ app/src/components/HeaderCollab/styles.js
@@ -2,11 +2,13 @@ import styled from "styled-components";
 
 export const Header = styled.header`
     background-color: var(--color-zero);
+    padding-top:20px;
     width: 80px;    
     height: 100vh;
     position: absolute;
     z-index:1;
     top:0;
     left:0;
+    text-align:center;
 `;
 
