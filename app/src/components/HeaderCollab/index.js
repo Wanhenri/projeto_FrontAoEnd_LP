7d153bb9cb735a5683f318e20ff8f3d71545ab02
wanhenri <slipkorn.jk@gmail.com>
2019-06-12 21:25:25 -0300

    modificado o NavigationCollab e acrescentado a const Items

--- app/src/components/HeaderCollab/index.js
+++ app/src/components/HeaderCollab/index.js
@@ -4,9 +4,16 @@ import { Header }  from "./styles";
 
 import LogoAvatar from "../../objects/LogoAvatar";
 
+import NavigationCollab from "../NavigationCollab";
+
+const Items = [
+    "0", "1", "2", "3", "4", "5"
+]
+
 const HeaderCollab = () => (
     <Header>
         <LogoAvatar />
+        <NavigationCollab items={ Items }/>
     </Header>
 );
 
