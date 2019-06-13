e1928d294a9a5e1df6cec82209e59b8622111a5e
wanhenri <slipkorn.jk@gmail.com>
2019-06-12 21:31:46 -0300

    criado o component Navigation, com seus index e styles configurados

---
+++ app/src/components/NavigationCollab/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import {Navigation, Item } from "./styles"
+
+const NavigationCollab = ({items}) => (
+    <Navigation>
+        {items.map(content => <Item>{content}</Item>)}    </Navigation>
+);
+
+export default NavigationCollab;
