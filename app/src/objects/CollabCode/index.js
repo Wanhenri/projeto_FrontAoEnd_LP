a2225f8db5abdc7f6cd4e0f8e4d656814da2c2a3
wanhenri <slipkorn.jk@gmail.com>
2019-07-09 19:41:46 -0300

    atualizado CollabCode

--- app/src/objects/CollabCode/index.js
+++ app/src/objects/CollabCode/index.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 import collabcode from "../../img/collabcode.png"
 
-const CollabCode = styled.img.attrs({src: collabcode })``;
+const CollabCode = styled.img.attrs({src: collabcode })`
+    height:inherit;
+`;
 
 export default CollabCode;
