22fa276cba81c6726c4fce24414422de49f9dfdf
wanhenri <slipkorn.jk@gmail.com>
2019-07-09 16:04:27 -0300

    modificação do nome para CollabCode

---
+++ app/src/objects/CollabCode/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+import collabcode from "../../img/collabcode.png"
+
+const CollabCode = styled.img.attrs({src: collabcode })``;
+
+export default CollabCode;
