a5f4ed957c8fd848e157067d7c4c7d10fd8aa249
wanhenri <slipkorn.jk@gmail.com>
2019-07-09 19:42:23 -0300

    atualizado o LogoCollab

--- app/src/objects/LogoCollab/styles.js
+++ app/src/objects/LogoCollab/styles.js
@@ -3,15 +3,11 @@ import styled from "styled-components";
 import AvatarCollab from "../AvatarCollab";
 
 export const Figure = styled.figure`
-    width:500px;
-    
-    &> * {
-        height:42px;
-    }
+    height:42px;
+
     &> ${AvatarCollab} {
         margin-right: var(--spacing-small);
-        float:left;
-        
+        float:left;        
     }
     
 `;
