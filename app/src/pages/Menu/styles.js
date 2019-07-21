34027448f59c072fea2e4294fa04bce7e96003cb
wanhenri <slipkorn.jk@gmail.com>
2019-07-21 18:25:36 -0300

    Menu styles atualizado

--- app/src/pages/Menu/styles.js
+++ app/src/pages/Menu/styles.js
@@ -1,6 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Section = styled.section`
     height: 100vh;
     background-color: var(--color-third);
-`;
+    padding-left: 500px;
+`
