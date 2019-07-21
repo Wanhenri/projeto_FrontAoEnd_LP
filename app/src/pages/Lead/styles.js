27d5413d214c04ec6937de20b44aba9c445e3503
wanhenri <slipkorn.jk@gmail.com>
2019-07-21 18:24:15 -0300

    atualizando Lead

--- app/src/pages/Lead/styles.js
+++ app/src/pages/Lead/styles.js
@@ -1,36 +1,34 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import SubtitleCollab from "../../objects/SubtitleCollab";
-import TextCollab from "../../objects/TextCollab";
-import VideoDofrontaoend from "../../components/VideoDofrontaoend";
-import { Form } from "../../components/FormLead/styles.js";
+import SubtitleCollab from '../../objects/SubtitleCollab'
+import TextCollab from '../../objects/TextCollab'
+import VideoDofrontaoend from '../../components/VideoDofrontaoend'
+import { Form } from '../../components/FormLead/styles'
 
 export const Section = styled.section`
-    display:flex;
+    display: flex;
     flex-direction: column;
-    align-items:center;
-    width:60%;
-    min-width: 545px; 
-    
+    align-items: center;
+    width: 60%;
+    min-width: 545px;
 
-    &> ${SubtitleCollab},
-    &> ${VideoDofrontaoend} {
+    & > ${SubtitleCollab}, & > ${VideoDofrontaoend} {
         margin-bottom: var(--spacing-big);
-    }  
+    }
 
-    &> ${SubtitleCollab} {
-        text-align:center;
+    & > ${SubtitleCollab} {
+        text-align: center;
         width: 500px;
     }
 
-    &> ${TextCollab}{
+    & > ${TextCollab} {
         margin-bottom: var(--spacing-small);
     }
 
-    &> ${Form}{
+    & > ${Form} {
         min-width: 545px;
     }
-`;
+`
 
 export const Wrapper = styled.div`
     display: flex;
@@ -38,4 +36,4 @@ export const Wrapper = styled.div`
     align-items: center;
     height: 100vh;
     background-color: var(--color-first);
-`;
+`
