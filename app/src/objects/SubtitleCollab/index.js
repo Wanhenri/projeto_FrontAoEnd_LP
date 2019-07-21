9e7721ade7ff5c175f48a31c26a06b6d10ba86c4
wanhenri <slipkorn.jk@gmail.com>
2019-07-21 18:22:21 -0300

    atualizando subtitleCollab

--- app/src/objects/SubtitleCollab/index.js
+++ app/src/objects/SubtitleCollab/index.js
@@ -1,8 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-const SubtitleCollab = styled.h2 `
+const SubtitleCollab = styled.h2`
+    font-weight: ${({ featered }) => (featered ? 'bold' : 'normal')}
     color: var(--color-zero);
     font-size: var(--size-medium);
-`;
+`
 
-export default SubtitleCollab;
+export default SubtitleCollab
