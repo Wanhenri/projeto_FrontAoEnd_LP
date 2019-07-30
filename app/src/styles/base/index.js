6a72ad68395f6dc516e78dbaf7b8cf31df357860
wanhenri <slipkorn.jk@gmail.com>
2019-07-29 23:27:37 -0300

    acrescimo do color no styles base index

--- app/src/styles/base/index.js
+++ app/src/styles/base/index.js
@@ -1,16 +1,16 @@
-import { createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 const Base = createGlobalStyle`
     html, body{  
         font-size:var(--size-base);      
         font-family: 'Roboto Slab', serif;
+        color: var(--color-base);
     }
 
     input {
         font-family:inherit;
         font-size: inherit;
     }
-`;
+`
 
-
-export default Base;
+export default Base
