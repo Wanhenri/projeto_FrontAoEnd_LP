f62f83969dd737ef7d641f186c190a6624566dda
wanhenri <slipkorn.jk@gmail.com>
2019-07-29 23:21:10 -0300

    novo component ListContent

---
+++ app/src/components/ListContent/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const List = styled.ol`
+    font-size: var(--size-medium);
+`
+
+export const Item = styled.li``
+
+export const Number = styled.span``
+
+export const Content = styled.span``
