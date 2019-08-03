bbd15df11ebf8d6ddb61c7d74add176d2c0f31f8
wanhenri <slipkorn.jk@gmail.com>
2019-08-03 16:34:51 -0300

    adiconado um novo size

--- app/src/styles/settings/size.js
+++ app/src/styles/settings/size.js
@@ -4,8 +4,8 @@ const Size = createGlobalStyle`
     :root {
         --radius-small: 4px;
 
-        --size-base:10px;
-        --size-very-small: 1.2rem;
+        --size-base:10px;        
+        --size-very-small: 1.5rem;
         --size-small:1.5rem;
         --size-medium:1.8rem;
         --size-half-big: 2rem;
